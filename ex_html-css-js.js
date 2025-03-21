5c9142e5cc44837d82b52895ab98330935cda68b
Alvaroo-19 <agargar1908b@g.educaand.es>
2025-03-21 14:19:13 +0100

    gbehnrtn

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,20 @@
 //Escribe aquí tu código JavaScript
+if (console.error){
+    prompt("Algunos campos están incompletos o no son correctos");
+}
+
+alert("¿Seguro que quieres confirmar y enviar tu pedido ahora");
+
+let usuario, pescado, calamares=6, adobo=7, boquerones=8;
+let precio 
+
+
+if (usuario = aceptar){
+    prompt("Formulario enviado");
+    alert("Gracias por tu pedido, Álvaro");
+} else {
+    prompt("No se envía");
+}
+
+prompt ("El precio total es" + precio)
+
